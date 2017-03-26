2edc2d767b85afa8691767e92ee8281c28d4d04f
Shivam Fialok <shivam.fialok98@gmail.com>
2017-03-26 09:43:45 +0530

    Updated index.html,style.css,weather.js to display search result and changed some appearance of web page

--- weather.js
+++ weather.js
@@ -9,8 +9,10 @@ $('#submitweather').click(function(){
 	 			type: "GET",
 	 			dataType: "jsonp",
 	 			success: function(data){
-	 				console.log(data);
-	 			
+	 				var widget = show(data); 
+
+	 				$("#show").html(widget);
+	 				$("#city").val('');
 	 		}
 
 	 	});
@@ -21,3 +23,16 @@ $('#submitweather').click(function(){
 
 });
 });
+
+function show(data){
+	return "<h3 style='font-size: 30px; font-weight: bold;' class='text-center'>Current Weather details for "+ data.name +"," + data.sys.country +"</h3>"+
+	"<h3><strong>Weather</strong>: "+ data.weather[0].main +"</h3>" +
+			"<h3 'padding-left:40px;'><strong>Description</strong>: <img src='http://openweathermap.org/img/w/"+data.weather[0].icon+".png'> "+ data.weather[0].description +"</h3>"+
+			"<h3 'padding-left:40px;'><strong>Temperature</strong>: "+ data.main.temp +"&deg;C</h3>"+
+			"<h3 'padding-left:40px;'><strong>Pressure</strong>: "+ data.main.pressure +" hPa</h3>"+
+			"<h3 'padding-left:40px;'><strong>Humidity</strong>: "+ data.main.humidity +"%</h3>"+
+			"<h3 'padding-left:40px;'><strong>Min. Temperature</strong>: "+ data.main.temp_min +"&deg;C</h3>"+
+			"<h3 'padding-left:40px;'><strong>Max. Temparature</strong>: "+ data.main.temp_max +"&deg;C</h3>"+
+			"<h3 'padding-left:40px;'><strong>Wind Speed</strong>: "+ data.wind.speed +" m/s</h3>"+
+			"<h3 'padding-left:40px;'><strong>Wind Direction</strong>: "+ data.wind.deg +"&deg;</h3>";
+}
